6e18e62928c18c084e9a2c36f0c71ce70f71a3a7
kay8181 <you@example.com>
2026-04-25 19:25:40 -0700

    Workbook 3, Search Inventory, Exercise 1

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,35 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        ArrayList<Product> inventory = getInventory();
+        System.out.println("We carry the following inventory: ");
+        for (int i = 0; i < inventory.size(); i++) {
+            Product p = inventory.get(i);
+            System.out.printf("Id: %d | %s - Price: $%.2f%n",
+                    p.getId(), p.getName(), p.getPrice());
         }
+
     }
+    public static ArrayList<Product> getInventory() {
+        ArrayList<Product> inventory = new ArrayList<Product>();
+
+        Product p1 = new Product(2222,"Katjes Gummies", 8.99);
+        Product p2 = new Product(3333,"iPhone Case", 15.99);
+        Product p3 = new Product(4444,"Pink Keyboard",49.99);
+        Product p4 = new Product(5555,"Mnemosyne Notebook", 15.99);
+        Product p5 = new Product(7777,"Sony Headphones", 150.99);
+
+        inventory.add(p1);
+        inventory.add(p2);
+        inventory.add(p3);
+        inventory.add(p4);
+        inventory.add(p5);
+
+
+        return inventory;
+    }
+
 }
