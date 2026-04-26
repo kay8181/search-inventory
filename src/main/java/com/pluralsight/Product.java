ec4d096f3bcf45c17ebc12d67ade72cd0b81fcca
kay8181 <you@example.com>
2026-04-25 19:26:13 -0700

    Workbook 3, Search Inventory, Exercise 1

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -39,12 +39,6 @@ public class Product {
 
     }
 
-        //@Override
-        //public String toString() {
-           // return "Product{" +
-                    //"name='" + name + '\'' +
-                    //", price=" + price +
-                    //'}';
 
 
 }
